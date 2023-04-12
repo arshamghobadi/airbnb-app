347248f133602ac133ae9fad11ab631a86d29434
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-04-12 10:23:35 +0100

    favorite section is compeleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
-export default function Home() {
-  return <div className="text-red-500"></div>;
+import getCurrentUser from './actions/getCurrentUser';
+import getListings from './api/listings/getListings';
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+        pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
+         xl:grid-cols-5 gap-8
+        "
+        >
+          {listings.map((listing) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
