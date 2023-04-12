c365c63130ec22f1a580928cf78bb3feb2154760
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-04-12 14:46:49 +0100

    bug map marker fixed

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -9,7 +9,7 @@ import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
-  IconRerinaUrl: markerIcon2x.src,
+  iconRetinaUrl: markerIcon2x.src,
   shadowUrl: markerShadow.src,
 });
 interface MapProps {
