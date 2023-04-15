2e76bbfea3ca1c2edbd75537f9834e07d0cf6912
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-04-15 19:16:41 +0100

    bug fixed

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth/next';
 
 import prisma from '../libs/prismadb';
-import { authOptions } from '@/pages/api/auth/[...nextAuth]';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 export async function getSession() {
   return await getServerSession(authOptions);
