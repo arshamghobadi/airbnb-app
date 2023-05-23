d203abf33c2e6c06ea69cb6ad8c452b4ae485c18
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-05-23 09:53:33 +0100

    model padding fix

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -65,8 +65,8 @@ const Modal: React.FC<ModalProps> = ({
         fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"
       >
         <div
-          className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5
-        my-6 mx-auto h-full lg:h-auto md:h-auto"
+          className="relative w-full pt-[680px] sm:pt-24 md:w-4/6 lg:w-3/6 xl:w-2/5
+        my-6 mx-auto  lg:h-auto md:h-auto"
         >
           <div
             className={`translate duration-300 h-full
